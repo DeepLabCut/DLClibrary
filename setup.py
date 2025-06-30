06ecf2570849386038f4ccdb1008832a6d7e3c46
AlexEMG <alexander@deeplabcut.org>
2025-07-01 01:19:49 +0200

    James Bond

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dlclibrary",
-    version="0.0.7",
+    version="0.0.8",
     author="A. & M. Mathis Labs",
     author_email="alexander@deeplabcut.org",
     description="Lightweight library supporting universal functions for the DeepLabCut ecosystem",
