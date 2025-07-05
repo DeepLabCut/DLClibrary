6e1e39fd238b020d24081bab79128dd65482607a
Alexander Mathis <alexander@deeplabcut.org>
2025-07-05 09:58:42 +0200

    Update test_pytorch_models.py

--- tests/test_pytorch_models.py
+++ tests/test_pytorch_models.py
@@ -16,12 +16,11 @@ import dlclibrary.dlcmodelzoo.modelzoo_download as modelzoo
 
 
 @pytest.mark.parametrize(
-    "data",
+    "detector_data",
     [
         ("superanimal_bird", ["ssdlite"]),
         ("superanimal_topviewmouse", ["fasterrcnn_resnet50_fpn_v2"]),
         ("superanimal_quadruped", ["fasterrcnn_resnet50_fpn_v2"]),
-        ("superanimal_humanbody", ["rtmpose_x"]),
     ]
 )
 def test_get_super_animal_detectors(data: tuple[str, list[str]]):
@@ -33,11 +32,12 @@ def test_get_super_animal_detectors(data: tuple[str, list[str]]):
 
 
 @pytest.mark.parametrize(
-    "data",
+    "posemodel_data",
     [
         ("superanimal_bird", ["resnet_50"]),
         ("superanimal_topviewmouse", ["hrnet_w32"]),
         ("superanimal_quadruped", ["hrnet_w32"]),
+        ("superanimal_humanbody", ["rtmpose_x"]),
     ]
 )
 def test_get_super_animal_pose_models(data: tuple[str, list[str]]):
